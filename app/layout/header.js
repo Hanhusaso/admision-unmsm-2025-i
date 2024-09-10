0ca86a774a6a3c9e11597026390fca5498ac26e7
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-09-10 13:04:06 -0500

    feat: crear el componente header

---
+++ app/layout/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header className="bg-primary text-white">
+			<div className="container mx-auto"></div>
+		</header>
+	)
+}
+
+export default Header
