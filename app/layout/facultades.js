7264d2cf007d6812cc1e3699a9edabd4da1fa39b
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-09-10 13:03:38 -0500

    feat: crear el componente facultades

---
+++ app/layout/facultades.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Facultades = () => {
+	return (
+		<div className="pt-[33px] pb-10 px-4">
+			<div className="container mx-auto">
+				<div className="flex flex-col items-center">
+					<h2 className="text-center text-primary text-lg font-black max-w-[224px]">Conoce nuestras facultades</h2>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Facultades
