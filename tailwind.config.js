73495cc832e1129cdebdee9e75d3bde242eea598
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-09-10 13:01:37 -0500

    feat: configurar fuente y colores personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				openSans: ['var(--font-open-sans)'],
+			},
+			colors: {
+				primary: {
+					DEFAULT: "var(--primary)",
+				},
+				secondary: {
+					DEFAULT: "var(--secondary)",
+				},
+				tertiary: {
+					DEFAULT: "var(--tertiary)",
+				},
+				gray: {
+					DEFAULT: "var(--gray)",
+				},
+			},
+		},
+	},
+	plugins: [],
 };
