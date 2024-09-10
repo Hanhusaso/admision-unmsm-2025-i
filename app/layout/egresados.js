ad3b370d5d1957d519f4c04d0b616c61bb4fb88b
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-09-10 13:03:26 -0500

    feat: crear el componente egresados

---
+++ app/layout/egresados.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Egresados = () => {
+	return (
+		<div className="pt-[33px] pb-5 px-4">
+			<div className="container mx-auto">
+				<div className="flex flex-col items-center">
+					<h2 className="text-center text-primary text-lg font-black max-w-[224px]">Escucha a nuestros egresados</h2>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Egresados
