fc5c554f55150fdacf891c702e6f6ed942b0dea4
Hanhusaso <hans.e.huiza.n@gmail.com>
2024-09-11 11:26:57 -0500

    feat(facultades): agregar videos

--- app/data/videos-facultades-por-areas.js
+++ app/data/videos-facultades-por-areas.js
@@ -2,91 +2,91 @@ const videosFacultadesPorAreas = {
 	"Ciencias Básicas": [
 		{
 			"facultad": "Ciencias Biológicas",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "6n2I8OESn5s",
 		},
-		{
-			"facultad": "Ciencias Físicas",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
-		},
+		// {
+		// 	"facultad": "Ciencias Físicas",
+		// 	"youtube_codigo_embed": "",
+		// },
 		{
 			"facultad": "Ciencias Matemáticas",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "wO7jtF0dIvc",
 		}
 	],
 	"Ciencias de la Salud": [
 		{
 			"facultad": "Farmacia y Bioquímica",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "NAflmTZKtd4",
 		},
 		{
 			"facultad": "Medicina Humana",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "5hw745TnN1I",
 		},
 		{
 			"facultad": "Medicina Veterinaria",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "VCLsVW7l_Ww",
 		},
 		{
 			"facultad": "Odontología",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "YJJ5-kVMOfI",
 		},
-		{
-			"facultad": "Psicología",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
-		}
+		// {
+		// 	"facultad": "Psicología",
+		// 	"youtube_codigo_embed": "ARAb9zvlI6k",
+		// }
 	],
 	"Ciencias Económicas y de la Gestión": [
 		{
 			"facultad": "Ciencias Administrativas",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "1srJ69QphpQ",
 		},
 		{
 			"facultad": "Ciencias Contables",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "EJbCn118D2A",
 		},
 		{
 			"facultad": "Ciencias Económicas",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "_BIKJq9AHlI",
 		}
 	],
 	"Humanidades y Ciencias Jurídicas y Sociales": [
 		{
 			"facultad": "Educación",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "lCk623DfdJg",
 		},
 		{
 			"facultad": "Ciencias Sociales",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "s_LsIeEbi_k",
 		},
 		{
 			"facultad": "Derecho y Ciencia Política",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "aLwhlM09dEI",
 		},
 		{
 			"facultad": "Letras y Ciencias Humanas",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "gv5BtQg21ag",
 		}
 	],
 	"Ingenierías": [
 		{
 			"facultad": "Ingeniería de Sistemas e Informática",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "rdbLhxHmhxE",
 		},
 		{
 			"facultad": "Ingeniería Electrónica y Eléctrica",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "UfF01eQDyjA",
 		},
 		{
 			"facultad": "Ingeniería Geológica, Minera, Metalúrgica y Geográfica",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "I50AQlNsdwQ",
 		},
-		{
-			"facultad": "Ingeniería Industrial",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
-		},
+		// {
+		// 	"facultad": "Ingeniería Industrial",
+		// 	"youtube_codigo_embed": "ARAb9zvlI6k",
+		// },
 		{
 			"facultad": "Química e Ingeniería Química",
-			"youtube_codigo_embed": "ARAb9zvlI6k",
+			"youtube_codigo_embed": "WJrbZyaHh50",
 		}
 	]
 }
